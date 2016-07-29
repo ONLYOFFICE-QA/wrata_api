f4673b823ecdd1b87bc9b839a613f6e685edc53e
Pavel Lobashov <shockwavenn@gmail.com>
2016-07-29 11:42:44 +0300

    Add ability to check if server is turn on

--- lib/wrata_api.rb
+++ lib/wrata_api.rb
@@ -20,7 +20,21 @@ module WrataApi
     # @return [Hash] Return servers data
     def server_data(server_name)
       uri = URI.parse("#{@uri}/runner/updated_data")
-      perform_get(uri, 'servers[]' => server_name)
+      response = perform_get(uri, 'servers[]' => server_name)
+      response['servers_data'].first
+    end
+
+    # @return [Symbol] if server is powered on or off
+    def powering_status(server_name)
+      all_data = server_data(server_name)
+      status = all_data['_status']
+      if status == 'created'
+        :on
+      elsif status == 'destroyed'
+        :off
+      else
+        :other
+      end
     end
 
     # Clear test in queue
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -4,7 +4,15 @@ describe 'My behaviour' do
   let(:api) { WrataApi::WrataApi.new }
 
   it 'WrataApi#servers_data responded' do
-    expect(api.server_data('nct-at-1')['servers_data'].length).to eq(1)
+    expect(api.server_data('nct-at-1')['name']).to eq('nct-at-1')
+  end
+
+  it 'WrataApi#powering_status return on' do
+    expect(api.powering_status('nct-at-1')).to eq(:on)
+  end
+
+  it 'WrataApi#powering_status return off' do
+    expect(api.powering_status('nct-at-2')).to eq(:off)
   end
 
   it 'WrataApi#clear_tests_list responded' do
