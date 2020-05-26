0e5bbf562490bce21d4499872f8cd2495307b9e4
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-05-26 14:16:57 +0300

    Cleanup `gemspec` file (#19)

---
+++ lib/wrata_api/name.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module WrataApi
+  # This module holds the WrataApi version information.
+  module Name
+    # @return [String] current gem version
+    STRING = 'wrata_api'
+  end
+end
