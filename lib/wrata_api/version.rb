7ac495e43ebdad8082903a5af07afe30eb1422f1
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-02-22 20:02:03 +0300

    Release v1.0.2 (#187)

--- lib/wrata_api/version.rb
+++ lib/wrata_api/version.rb
@@ -4,6 +4,6 @@ module WrataApi
   # This module holds the WrataApi version information.
   module Version
     # @return [String] current gem version
-    STRING = '1.0.1'
+    STRING = '1.0.2'
   end
 end
